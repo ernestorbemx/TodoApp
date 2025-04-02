7f917fe910ac40595dc849314882e98e15d93155
ernestorbemx <ernesto.ramirez@encora.com>
2025-04-01 22:37:22 -0600

    Add Todo Service & Todo Controller

---
+++ src/main/java/com/encora/ernesto/ramirez/todo_app/controllers/TodoController.java
@@ -0,0 +1,56 @@
+package com.encora.ernesto.ramirez.todo_app.controllers;
+
+import com.encora.ernesto.ramirez.todo_app.dtos.Pagination;
+import com.encora.ernesto.ramirez.todo_app.dtos.TodoDto;
+import com.encora.ernesto.ramirez.todo_app.dtos.TodoFilter;
+import com.encora.ernesto.ramirez.todo_app.models.Todo;
+import com.encora.ernesto.ramirez.todo_app.services.TodoService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController()
+@RequestMapping("/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public Todo createTodo(@Valid @RequestBody TodoDto dto) {
+        return this.todoService.createTodo(dto);
+    }
+
+    @PutMapping("/{id}")
+    public Optional<Todo> createTodo(@PathVariable("id") int id, @Valid @RequestBody TodoDto dto) {
+        return this.todoService.editTodo(id, dto);
+    }
+
+
+    @PutMapping("/{id}/done")
+    public Optional<Todo> markAsDone(@PathVariable("id") int id) {
+        return this.todoService.editTodoCompletion(id, true);
+    }
+
+    @PutMapping("/{id}/undone")
+    public Optional<Todo> markAsUndone(@PathVariable("id") int id) {
+        return this.todoService.editTodoCompletion(id, false);
+    }
+
+    @GetMapping
+    public List<Todo> getTodos(@Valid @ModelAttribute TodoFilter dto, @Valid @ModelAttribute Pagination paginationDto) {
+        System.out.println(dto.toString());
+        System.out.println(paginationDto.toString());
+        return this.todoService.getTodos(dto, paginationDto);
+    }
+
+    @GetMapping("/example")
+    public String example() {
+        return "Hola";
+    }
+}
---
+++ src/main/java/com/encora/ernesto/ramirez/todo_app/services/TodoService.java
@@ -0,0 +1,59 @@
+package com.encora.ernesto.ramirez.todo_app.services;
+
+import com.encora.ernesto.ramirez.todo_app.dtos.Pagination;
+import com.encora.ernesto.ramirez.todo_app.dtos.TodoDto;
+import com.encora.ernesto.ramirez.todo_app.dtos.TodoFilter;
+import com.encora.ernesto.ramirez.todo_app.models.Todo;
+import com.encora.ernesto.ramirez.todo_app.repositories.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TodoService {
+
+    private static final int PAG_SIZE = 10;
+
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> getTodos(TodoFilter filter, Pagination pagination) {
+        return this.todoRepository.getTodos(filter, pagination);
+    }
+
+    public Todo createTodo(TodoDto todo) {
+        Todo newTodo = new Todo(todo.getText(), todo.getDueDate(), todo.getPriority(), LocalDateTime.now());
+        this.todoRepository.save(newTodo);
+        return newTodo;
+    }
+
+    // Edition is not guaranteed
+    public Optional<Todo> editTodo(int id, TodoDto dto) {
+        Optional<Todo> ot = this.todoRepository.findById(id);
+        if(ot.isPresent()) {
+            Todo t = ot.get();
+            t.setText(dto.getText());
+            t.setDueDate(dto.getDueDate());
+            t.setPriority(dto.getPriority());
+            return Optional.of(t);
+        }
+        return Optional.empty();
+    }
+
+    public Optional<Todo> editTodoCompletion(int id, boolean done) {
+        Optional<Todo> ot = this.todoRepository.findById(id);
+        if(ot.isPresent()) {
+            Todo t = ot.get();
+            t.setDone(done);
+            t.setCompletionDate(done ? LocalDateTime.now() : null);
+            return Optional.of(t);
+        }
+        return Optional.empty();
+    }
+
+}
