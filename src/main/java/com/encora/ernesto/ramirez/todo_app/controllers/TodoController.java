1c4790419773e83c697304479da00e5cb8c6e8d2
ernestorbemx <ernesto.ramirez@encora.com>
2025-04-03 23:14:09 -0600

    Enable Cors on http://localhost:8080

--- src/main/java/com/encora/ernesto/ramirez/todo_app/controllers/TodoController.java
+++ src/main/java/com/encora/ernesto/ramirez/todo_app/controllers/TodoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = {"http://localhost:8080", "http://127.0.0.1:8080"})
 @RestController()
 @RequestMapping("/todos")
 public class TodoController {
